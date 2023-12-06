f70bdbad9971bd2f39f53c27967d1b67ace0b989
Johanna Selander <johanna.selander.JAVA23@edu.edugrade.se>
2023-12-06 16:10:10 +0100

    Ny commit efter jag testat att det funkar i Mysql

--- src/main/java/org/teamBravo/Main.java
+++ src/main/java/org/teamBravo/Main.java
@@ -2,11 +2,17 @@ package org.teamBravo;
 
 import org.teamBravo.controller.CustomerController;
 import org.teamBravo.controller.PersonController;
+import org.teamBravo.entity.Person;
 import org.teamBravo.view.Menu;
+
+import java.util.List;
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
         PersonController personController = new PersonController();
+
+
         /*CustomerController controller = new CustomerController();
         Menu menu = new Menu(controller);
         menu.showMenu();*/
